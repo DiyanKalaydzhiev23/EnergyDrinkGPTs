773a2c14877abe3cddfef875a5bbf821fbd00837
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2024-05-27 20:10:54 +0300

    chore: fixed CongratsView

--- EnergyDrinksGPTs/app/src/main/java/com/example/enrgydrinksgpts/energyDrinksCards/CongratsView.java
+++ EnergyDrinksGPTs/app/src/main/java/com/example/enrgydrinksgpts/energyDrinksCards/CongratsView.java
@@ -1,11 +1,14 @@
 package com.example.enrgydrinksgpts.energyDrinksCards; // Replace with your actual package name
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.example.enrgydrinksgpts.MainActivity;
 import com.example.enrgydrinksgpts.R;
 
 public class CongratsView extends AppCompatActivity {
@@ -20,14 +23,21 @@ public class CongratsView extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.energyDrinkCongrats);
         Glide.with(this)
                 .load(bestMatchUrl)
-//                .placeholder(R.drawable.loading_placeholder) // optional
                 .into(imageView);
-
 
         ImageView gifImageView = findViewById(R.id.gifImageView);
         Glide.with(this)
                 .asGif()
                 .load(R.drawable.confeti)
                 .into(gifImageView);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(CongratsView.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
